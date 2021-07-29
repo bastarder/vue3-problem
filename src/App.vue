d6aee24fe129b1a928e58e13816dda7647ce5aca
qianjie <qianjie@patsnap.com>
2021-07-29 09:25:27 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    {{ b }}
+  </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, reactive } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    const refA = ref(1);
+    const reactiveB = reactive({ k: refA });
+    console.log(reactiveB.k)
+
+    setTimeout(() => {
+      refA.value = Math.random();
+    }, 5000);
+    return {
+      b: reactiveB
+    }
+
+  }
+})
+</script>
+
+<style>
+#app {
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
